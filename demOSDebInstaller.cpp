7dadf45685e156b4aa2188971a03cfd2c1ea663f
hhk02 <80901734+hhk02@users.noreply.github.com>
2023-02-03 05:07:44 +0100

    Update demOSDebInstaller.cpp

--- demOSDebInstaller.cpp
+++ demOSDebInstaller.cpp
@@ -52,7 +52,7 @@ void InstallProcess()
     		string exec5 = "grub-install --target=i386-pc --root-directory=/media/target/ " + disk;
     		system(exec5.c_str());
 		// Cambiar a la instalación de destino y ejecutar update-grub para generar la configuración del GRUB
-        	cout << "Use grub-install --target=i386-pc --root-directory=/media/target/  genfstab -U /media/target/ >> /media/target/etc/fstab in the other termianl and finally in the same window of the installer write update-grub\n" << endl;
+        	cout << "Use genfstab -U /media/target/ >> /media/target/etc/fstab in the other termianl.\n Then run update-initramfs -u in the same terminal where you ran the installer. \n and finally in the same window of the installer write update-grub\n" << endl;
 		system("chroot /media/target");
 		cout << "Installation complete!" << endl;
 
@@ -65,7 +65,7 @@ void InstallProcess()
 		system("clear");
 		cout << "FIRST COMMAND: You are right now in the new installation of DemenciaOS (chroot).\n put mkdir -v /mnt/boottemp and cp -rv /boot /mnt/boottemp\n" << endl;
 		cout << "SECOND COMMAND: put mount /dev/sdx1 /boot or /dev/nvme0n1p1 /boot (NVMe) and grub-install --target=x86_64-efi --efi-directory=/boot\n, open an other terminal and login with root with sudo -i or sudo su and write genfstab -U /media/target/ >> /media/target/etc/fstab\n" << endl;
-		cout << "THIRD COMMAND: put cp -rv /mnt/boottemp/boot/* /boot/ and finally. put update-grub and finally use command to exit. \n " << endl;
+		cout << "THIRD COMMAND: put cp -rv /mnt/boottemp/boot/* /boot/ Then run update-initramfs -u in the same terminal where you ran the installer. and finally. put update-grub and finally use command to exit. \n " << endl;
 		system("chroot /media/target");
         	cout << "Installation complete you can reboot with sudo reboot or systemctl reboot" << endl;
 
